50a2bb1f3bc0197cf59f7a0af3789e617b214365
blessador <blessador@gmail.com>
2026-07-15 21:59:46 +0100

    Update network-health.sh

--- Day-02-Linux-Networking/network-health.sh
+++ Day-02-Linux-Networking/network-health.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+
+echo "========================"
+echo "NETWORK HEALTH REPORT"
+echo "========================"
+
+echo "Hostname : $(hostname)"
+
+echo "IP Address : $(hostname -I)"
+
+echo "Gateway : $(ip route)"
+
+echo "DNS Server : $(cat /etc/resolv.conf)"
+
+echo "Internet Connectivity : $(ping -c 4 google.com)"
+
+echo "Listening Ports : $(nmap localhost)"
+
+echo "Open SSH Status : $(ss -tuln)"
+
+echo "Current Date : $(date)"
+
+echo "========================"
 
